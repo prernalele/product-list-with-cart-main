511b57493e6107e4bbc57f8117b285d1d3b91356
Prerna Lele <prernalele@gmail.com>
2024-10-09 15:36:41 -0400

    feat: fetches data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import data from '../data.json'
 import Dessert from "./components/Dessert";
 
 const  App = () => {
+    const [allData, setAllData] = useState(null)
+useEffect (()=> {
+    fetch(data)
+    .then(setAllData(data))
+    .catch((error) => console.log("Error fetching data", error));
+},[])
 
 return (
     <div className="flex bg-rose-50 h-screen">
-        <Dessert />
+        <Dessert data={allData}/>
     </div>
     )
 
--- src/components/Dessert.jsx
+++ src/components/Dessert.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 
-const Dessert = () => {
+const Dessert = ({data}) => {
+    console.log("data", data)
     const pageHeading = "Desserts"
 
     return (
-        <div className="@font-face m-10 h-screen w-1/2">
-            <h2 className="@font-face bold m-14"> {pageHeading}</h2>
+        <div className="@font-face mx-14 h-screen w-1/2">
+            <h2 className="@font-face text-lg font-bold m-14"> {pageHeading}</h2>
         </div>
         
     )
